0df747dba652c6f32884995f34bc85f7ecf28796
Mohammad <jawid.dr971@gmail.com>
2025-10-01 14:16:23 -0400

    Adding Calculator project, gitignore File

---
+++ LearningGit/src/com/git/learning/Calculator.java
@@ -0,0 +1,12 @@
+package com.git.learning;
+
+public class Calculator {
+	
+	// this is like we join the company and company has nothing we start this brand new
+	// project from beginning.
+	
+	// things we need // they will send you link for joining GithUB
+	//1- login credentials for GitHub.(user_name + password)  goto GitHub.come enter userName+password sign up/sign in
+	//
+
+}
