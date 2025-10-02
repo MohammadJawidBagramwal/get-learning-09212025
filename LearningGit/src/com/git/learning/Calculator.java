b4b4854b5d7b26e10f9985051ed5ee9332271ff1
Mohammad <jawid.dr971@gmail.com>
2025-10-02 17:55:20 -0400

    adding add method

--- LearningGit/src/com/git/learning/Calculator.java
+++ LearningGit/src/com/git/learning/Calculator.java
@@ -1,6 +1,6 @@
 package com.git.learning;
 
-public class Calculator {
+public class Calculator {// in project name firstone is projectName second is repo name 3 master branch
 	
 	// this is like we join the company and company has nothing we start this brand new
 	// project from beginning.
@@ -9,4 +9,8 @@ public class Calculator {
 	//1- login credentials for GitHub.(user_name + password)  goto GitHub.come enter userName+password sign up/sign in
 	//
 
+	
+	public int add(int x,int y) {
+		return x+y;
+	}
 }
